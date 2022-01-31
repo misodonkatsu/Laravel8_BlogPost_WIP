548baba205806faa4b6b5ce245ca6e336b31f002
misodonkatsu <76638714+misodonkatsu@users.noreply.github.com>
2022-02-01 00:51:49 +0900

    Revert "add Laravel project"
    This reverts commit 09ac7e082363097dd53843d048791a31c345cc7c.

--- resources/views/home/contact.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Contact page')
-
-@section('content')
-    <h1>Contact</h1>
-    <p>This is the Contact Page</p>
-@endsection
--- resources/views/home/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Home page')
-
-@section('content')
-    <h1>Welcome to Laravel!</h1>
-    <p>This is the content of the main page!</p>
-@endsection
